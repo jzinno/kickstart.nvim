6f0204c44419c97562cee05b1a5b265803aa2d17
John Zinno <johnzinno14@gmail.com>
2024-08-29 10:07:22 -0400

    update avante.nvim install

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,8 +6,34 @@ return {
   {
     'yetone/avante.nvim',
     event = 'VeryLazy',
+    lazy = false,
     opts = {
       -- add any opts here
+    },
+    keys = {
+      {
+        '<leader>aa',
+        function()
+          require('avante.api').ask()
+        end,
+        desc = 'avante: ask',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>ar',
+        function()
+          require('avante.api').refresh()
+        end,
+        desc = 'avante: refresh',
+      },
+      {
+        '<leader>ae',
+        function()
+          require('avante.api').edit()
+        end,
+        desc = 'avante: edit',
+        mode = 'v',
+      },
     },
     dependencies = {
       'stevearc/dressing.nvim',
@@ -16,6 +42,23 @@ return {
       --- The below dependencies are optional,
       'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
       {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
         -- Make sure to setup it properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {
@@ -23,18 +66,6 @@ return {
         },
         ft = { 'markdown', 'Avante' },
       },
-    },
-  },
-  {
-    'HakonHarnes/img-clip.nvim',
-    event = 'VeryLazy',
-    opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
-    },
-    keys = {
-      -- suggested keymap
-      { '<leader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
     },
   },
 }
